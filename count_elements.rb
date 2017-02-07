c34924238f60f2787e9aa1522fe658c9c1d667e1
CorinneKelly <corinnemariekelly@gmail.com>
2017-02-06 22:44:53 -0500

    solutions

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  hash = {}
+  array.map do |animal|
+  	hash[animal] = count_animal(animal, array)
+  end
+  hash
+end
+
+def count_animal(animal, array)
+	array.count(animal)
 end
  
